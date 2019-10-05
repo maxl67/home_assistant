9b63c81e9644b9e2798097fda67c1b866d423a6d
Max Reigl <Max@Nemesis.fritz.box>
2019-10-05 22:47:41 +0200

    in der Entwicklung von Vacuum Pause/Resume

---
+++ www/community/lovelace-xiaomi-vacuum-map-card/texts.js
@@ -0,0 +1,17 @@
+const textMode = "Mode";
+const textGoToTarget = "Go to target";
+const textZonedCleanup = "Zoned cleanup";
+const textZones = "Zones";
+const textRun = "Start";
+const textRepeats = "Times:";
+const textConfirmation = "Command sent!";
+
+export {
+    textMode,
+    textGoToTarget,
+    textZonedCleanup,
+    textZones,
+    textRun,
+    textRepeats,
+    textConfirmation
+};
